430b1086afc29d70d579768da166f30532b07e61
Victor Felder <victor@draft.li>
2017-03-18 17:09:12 +0100

    Rename files to make sense

--- packages/html-blocks/index.js
+++ packages/html-blocks/index.js
@@ -1,3 +1,29 @@
-const inlinePlugin = require('./inline')
+const visit = require('unist-util-visit')
 
-module.exports = inlinePlugin
+function plugin () {
+  return transformer
+}
+
+function transformer (tree) {
+  visit(tree, 'raw', visitor)
+}
+
+function visitor (node) {
+  node.type = 'text'
+  if (node.position.start.line !== node.position.end.line) {
+    node.type = 'element'
+    node.tagName = 'p'
+    node.properties = {}
+    node.children = [
+      {
+        type: 'text',
+        value: node.value,
+        position: node.position
+      }
+    ]
+    node.value = undefined
+    node.position = undefined
+  }
+}
+
+module.exports = plugin
