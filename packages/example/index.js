4df1df4aed4c9a0c1d7d7941db14a27c614aaecb
Victor Felder <victor@draft.li>
2017-03-19 18:35:11 +0100

    Remove example plugin

--- packages/example/index.js
+++
@@ -1,8 +0,0 @@
-// https://github.com/Rokt33r/remark-math/blob/master/packages/remark-math/index.js
-const inlinePlugin = require('./inline')
-const blockPlugin = require('./block')
-
-module.exports = [
-  [inlinePlugin],
-  [blockPlugin]
-]
