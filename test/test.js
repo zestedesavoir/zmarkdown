106bcb6bd19e1dff07367a8f200fb816e4dc64bc
Victor Felder <victor@draft.li>
2017-03-19 17:25:50 +0100

    Fix russian test; remove a useless test

--- test/test.js
+++ test/test.js
@@ -204,11 +204,6 @@ describe('HTML rendering', function () {
       expect(renderFile(filepath)).to.have.html(loadFixture(filepath))
     })
 
-    it.skip(`properly renders autolinks_with_asterisks_russian.txt`, function () {
-      const filepath = `${dir}/autolinks_with_asterisks_russian.txt`
-      expect(renderFile(filepath)).to.have.html(loadFixture(filepath))
-    })
-
     it.skip(`properly renders autolinks_with_asterisks.txt`, function () {
       const filepath = `${dir}/autolinks_with_asterisks.txt`
       expect(renderFile(filepath)).to.have.html(loadFixture(filepath))
@@ -529,7 +524,7 @@ describe('HTML rendering', function () {
       expect(renderFile(filepath)).to.have.html(loadFixture(filepath))
     })
 
-    it.skip(`properly renders russian.txt`, function () {
+    it(`properly renders russian.txt`, function () {
       const filepath = `${dir}/russian.txt`
       expect(renderFile(filepath)).to.have.html(loadFixture(filepath))
     })
