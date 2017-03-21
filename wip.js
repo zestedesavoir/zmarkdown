b9cd831781d02a330d2597856b4090d5b0c6b54d
victor felder <victor@draft.li>
2017-03-21 22:19:20 +0100

    Devtool (#23)

---
+++ wip.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-console */
+const { renderString, parse, inspect } = require('./index.js')
+
+/*
+1. Write a plugin
+2. `.use()` is in `processor` in `index.js`
+3. `npm run wip`
+4. Changing the markdown below or modifying your plugin will refresh your terminal
+*/
+
+const markdown = `
+# hello
+`
+
+const now = String(new Date())
+const dash = '='.repeat(Math.floor((80 - now.length) / 2))
+console.log(`\n${dash}${now}${dash}${dash % 2 ? '' : '='}\n`)
+
+console.log(renderString(markdown))
+
+console.log(`\n${'-'.repeat(80)}\n`)
+
+console.error(inspect(parse(markdown)))
