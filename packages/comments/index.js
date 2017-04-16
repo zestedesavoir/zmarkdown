c1389e8e36ff48d720f314b91d296bf3605e8946
AmarOk <AmarOk1412@users.noreply.github.com>
2017-04-16 04:27:20 -0400

    implement zds extension: comments (#43)
    * implement zds extension: comments
    
    * style

---
+++ packages/comments/index.js
@@ -0,0 +1,32 @@
+const BEGINMARKER = '<--COMMENTS'
+const ENDMARKER = 'COMMENTS-->'
+const SPACE = ' '
+
+function locator (value, fromIndex) {
+  return value.indexOf(BEGINMARKER, fromIndex)
+}
+
+function inlinePlugin (opts = {}) {
+  function inlineTokenizer (eat, value, silent) {
+
+    const keepBegin = value.indexOf(BEGINMARKER)
+    const keepEnd = value.indexOf(ENDMARKER)
+    if (keepBegin !== 0 || keepEnd === -1) return
+
+    if (silent) return silent
+
+    const comment = value.substring(BEGINMARKER.length + 1, keepEnd - 1)
+    return eat(BEGINMARKER + SPACE + comment + SPACE + ENDMARKER)
+  }
+  inlineTokenizer.locator = locator
+
+  const Parser = this.Parser
+
+  // Inject inlineTokenizer
+  const inlineTokenizers = Parser.prototype.inlineTokenizers
+  const inlineMethods = Parser.prototype.inlineMethods
+  inlineTokenizers.comments = inlineTokenizer
+  inlineMethods.splice(inlineMethods.indexOf('text'), 0, 'comments')
+}
+
+module.exports = inlinePlugin
