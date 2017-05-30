ce99cc46d15e60c53b1bab798bba5d69d88b0bfc
Victor Felder <victor@draft.li>
2017-05-30 22:33:56 +0200

    remark-custom-blocks tests

---
+++ packages/remark-custom-blocks/__tests__/index.js
@@ -0,0 +1,45 @@
+import {readdirSync as directory, readFileSync as file} from 'fs'
+import {join} from 'path'
+import ava from 'ava'
+import plugin from '..'
+import unified from 'unified'
+import reParse from 'remark-parse'
+import stringify from 'rehype-stringify'
+import remark2rehype from 'remark-rehype'
+
+const base = join(__dirname, 'fixtures')
+
+const specs = directory(base).reduce((tests, contents) => {
+  const parts = contents.split('.')
+  if (!tests[parts[0]]) {
+    tests[parts[0]] = {}
+  }
+  tests[parts[0]][parts[1]] = file(join(base, contents), 'utf-8')
+  return tests
+}, {})
+
+Object.keys(specs).filter(Boolean).forEach(name => {
+  const spec = specs[name]
+
+  ava(name, t => {
+    const {contents} = unified()
+      .use(reParse)
+      .use(remark2rehype)
+      .use(plugin, {
+        secret: 'spoiler',
+        s: 'spoiler',
+        information: 'information ico-after',
+        i: 'information ico-after',
+        question: 'question ico-after',
+        q: 'question ico-after',
+        attention: 'warning ico-after',
+        a: 'warning ico-after',
+        erreur: 'error ico-after',
+        e: 'error ico-after'
+      })
+      .use(stringify)
+      .processSync(spec.fixture)
+
+    t.deepEqual(contents, spec.expected.trim())
+  })
+})
