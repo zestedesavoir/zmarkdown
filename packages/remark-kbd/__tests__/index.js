db97c8e6cb0a8f7a3ede565e4c34e5a85152e091
Victor Felder <victor@draft.li>
2017-05-31 18:35:03 +0200

    remark-kbd: tests

---
+++ packages/remark-kbd/__tests__/index.js
@@ -0,0 +1,48 @@
+import {readdirSync as directory, readFileSync as file} from 'fs'
+import {join} from 'path'
+import ava from 'ava'
+import plugin from '..'
+import unified from 'unified'
+import reParse from 'remark-parse'
+import remarkStringify from 'remark-stringify'
+import rehypeStringify from 'rehype-stringify'
+import remark2rehype from 'remark-rehype'
+import remarkCustomBlocks from '../../remark-custom-blocks'
+
+const base = join(__dirname, 'fixtures')
+
+const specs = directory(base).reduce((tests, contents) => {
+  const parts = contents.split('.')
+  if (!tests[parts[0]]) {
+    tests[parts[0]] = {}
+  }
+  tests[parts[0]][parts[1]] = file(join(base, contents), 'utf-8')
+  return tests
+}, {})
+
+
+ava('kbd', t => {
+  const {contents} = unified()
+    .use(reParse, {
+      footnotes: true
+    })
+    .use(remark2rehype)
+    .use(remarkCustomBlocks, {
+      secret: 'spoiler'
+    })
+    .use(plugin)
+    .use(rehypeStringify)
+    .processSync(specs['kbd'].fixture)
+
+  t.deepEqual(contents, specs['kbd'].expected.trim())
+})
+
+ava('to markdown', t => {
+  const {contents} = unified()
+    .use(reParse)
+    .use(remarkStringify)
+    .use(plugin)
+    .processSync(specs['kbd'].fixture)
+
+  t.deepEqual(contents, specs['md'].fixture)
+})
