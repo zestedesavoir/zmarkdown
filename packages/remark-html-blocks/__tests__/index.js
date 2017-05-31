3cf8b0182c0a1392f1b59ada89321919879792b7
Victor Felder <victor@draft.li>
2017-05-31 22:29:10 +0200

    remark-html-blocks: final fix

---
+++ packages/remark-html-blocks/__tests__/index.js
@@ -0,0 +1,67 @@
+import {readdirSync as directory, readFileSync as file} from 'fs'
+import {join} from 'path'
+import ava from 'ava'
+import plugin from '..'
+import unified from 'unified'
+import reParse from 'remark-parse'
+import rehypeStringify from 'rehype-stringify'
+import remark2rehype from 'remark-rehype'
+
+const base = join(__dirname, 'fixtures')
+
+const specs = directory(base).reduce((tests, contents) => {
+  const parts = contents.split('.')
+  if (!tests[parts[0]]) {
+    tests[parts[0]] = {}
+  }
+  tests[parts[0]][parts[1]] = file(join(base, contents), 'utf-8')
+  return tests
+}, {})
+
+
+Object.keys(specs).filter(Boolean).filter(name => !name.endsWith('-without')).forEach(name => {
+  const spec = specs[name]
+
+  ava(name, t => {
+    const {contents} = unified()
+      .use(reParse, {
+        gfm: true,
+        commonmark: false,
+        yaml: false,
+        footnotes: true,
+        /* sets list of known blocks to nothing, otherwise <h3>hey</h3> would become
+        &#x3C;h3>hey&#x3C;/h3> instead of <p>&#x3C;h3>hey&#x3C;/h3></p> */
+        blocks: [],
+      })
+      .use(remark2rehype, { allowDangerousHTML: true })
+      .use(plugin)
+      .use(rehypeStringify)
+      .processSync(spec.fixture)
+
+    t.deepEqual(contents, spec.expected.trim())
+  })
+})
+
+
+Object.keys(specs).filter(Boolean).filter(name => !name.endsWith('-without')).forEach(name => {
+  const spec = specs[name]
+
+  ava(name, t => {
+    const {contents} = unified()
+      .use(reParse, {
+        gfm: true,
+        commonmark: false,
+        yaml: false,
+        footnotes: true,
+        /* sets list of known blocks to nothing, otherwise <h3>hey</h3> would become
+        &#x3C;h3>hey&#x3C;/h3> instead of <p>&#x3C;h3>hey&#x3C;/h3></p> */
+        blocks: [],
+      })
+      .use(remark2rehype, { allowDangerousHTML: true })
+      // .use(plugin)
+      .use(rehypeStringify)
+      .processSync(spec.fixture)
+
+    t.deepEqual(contents, specs[`${name}-without`].expected.trim())
+  })
+})
---
+++ packages/remark-html-blocks/dist/index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var visit = require('unist-util-visit');
+
+var inline = ['a', 'b', 'big', 'i', 'small', 'tt', 'abbr', 'acronym', 'cite', 'code', 'dfn', 'em', 'kbd', 'strong', 'samp', 'time', 'var', 'bdo', 'br', 'img', 'map', 'object', 'p', 'q', 'script', 'span', 'sub', 'sup', 'button', 'input', 'label', 'select', 'textarea'];
+
+function plugin() {
+  return transformer;
+}
+
+function transformer(tree) {
+  visit(tree, 'raw', visitor);
+}
+
+function visitor(node, index, parent) {
+  var replacement = void 0;
+  // if (!parent) return
+
+  if (!inline.includes(parent.tagName)) {
+    replacement = {
+      type: 'element',
+      tagName: 'p',
+      properties: {},
+      children: [{
+        type: 'text',
+        value: node.value
+      }]
+    };
+  } else {
+    replacement = {
+      type: 'text',
+      value: node.value
+    };
+  }
+
+  parent.children[index] = replacement;
+}
+
+module.exports = plugin;
