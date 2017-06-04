ee4363961ce9588efbc91e1e10e1646b9cf1a440
Victor Felder <victor@draft.li>
2017-06-04 23:17:46 +0200

    remark-numbered-footnotes: add failing fixtures

--- packages/remark-numbered-footnotes/__tests__/index.js
+++ packages/remark-numbered-footnotes/__tests__/index.js
@@ -30,7 +30,13 @@ entrypoints.forEach(entrypoint => {
     ava.skip(name, t => {
       const {contents} = unified()
         .use(reParse, {
-          footnotes: true
+          gfm: true,
+          commonmark: false,
+          yaml: false,
+          footnotes: true,
+          /* sets list of known blocks to nothing, otherwise <h3>hey</h3> would become
+          &#x3C;h3>hey&#x3C;/h3> instead of <p>&#x3C;h3>hey&#x3C;/h3></p> */
+          blocks: [],
         })
         .use(plugin)
         .use(remark2rehype)
