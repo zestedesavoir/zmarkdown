09727fcfc2bc641ae4b74943cc517e3a73feefde
AmarOk <contact@enconn.fr>
2017-06-04 14:43:38 -0400

    abbr plugin should visit all nodes

--- packages/rehype-abbr/dist/index.js
+++ packages/rehype-abbr/dist/index.js
@@ -12,7 +12,7 @@ function plugin() {
     var keep = regex.exec(value);
 
     /* istanbul ignore if - never used (yet) */
-    if (silent) return true;
+    if (silent) return silent;
     if (!keep || keep.index !== 0) return;
 
     return eat(keep[0])({
@@ -70,29 +70,36 @@ function plugin() {
     var regex = new RegExp('\\b(' + pattern + ')\\b');
 
     function one(node, index, parent) {
-
       if (Object.keys(abbrs).length === 0) return;
-      if (node.type !== 'text') return;
-      if (parent && parent.tagName === 'abbr') return;
+      if (!node.children) return;
 
-      var keep = regex.exec(node.value);
-      if (keep) {
-        var newTexts = node.value.split(regex);
-        parent.children.splice(index, 1);
-        for (var i = 0; i < newTexts.length; ++i) {
-          var content = newTexts[i];
-          if (abbrs.hasOwnProperty(content)) {
-            parent.children.splice(index + i, 0, {
-              type: 'element',
-              tagName: 'abbr',
-              properties: { title: abbrs[content] },
-              children: [{ type: 'text', value: content }]
-            });
-          } else {
-            parent.children.splice(index + i, 0, {
-              type: 'text',
-              value: content
-            });
+      // If a text node is present in child nodes, check if an abbreviation is present
+      for (var c = 0; c < node.children.length; ++c) {
+        var child = node.children[c];
+        if (node.tagName !== 'abbr' && child.type === 'text') {
+          var keep = regex.exec(child.value);
+          if (keep) {
+            // Transform node
+            var newTexts = child.value.split(regex);
+            // Remove old text node
+            node.children.splice(c, 1);
+            // Replace abbreviations
+            for (var i = 0; i < newTexts.length; ++i) {
+              var content = newTexts[i];
+              if (abbrs.hasOwnProperty(content)) {
+                node.children.splice(c + i, 0, {
+                  type: 'element',
+                  tagName: 'abbr',
+                  properties: { title: abbrs[content] },
+                  children: [{ type: 'text', value: content }]
+                });
+              } else {
+                node.children.splice(c + i, 0, {
+                  type: 'text',
+                  value: content
+                });
+              }
+            }
           }
         }
       }
