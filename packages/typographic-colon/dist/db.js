10d0c27b6089530d8b701c7dae9b74d802061c42
Victor Felder <victor@draft.li>
2017-06-06 23:04:33 +0200

    typographic-colon: fix unicode chars

--- packages/typographic-colon/dist/db.js
+++ packages/typographic-colon/dist/db.js
@@ -1,7 +1,11 @@
 'use strict';
 
+var chars = {
+  'NARROW NO-BREAK SPACE': '\u202F'
+};
+
 // Language codes:     https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
 module.exports = {
-  'fr': ' ',
-  'fr-sw': ' '
+  'fr': chars['NARROW NO-BREAK SPACE'],
+  'fr-sw': chars['NARROW NO-BREAK SPACE']
 };
--- packages/typographic-colon/src/db.js
+++ packages/typographic-colon/src/db.js
@@ -1,5 +1,9 @@
+const chars = {
+  'NARROW NO-BREAK SPACE': '\u202F'
+}
+
 // Language codes:     https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
 module.exports = {
-  'fr': ' ',
-  'fr-sw': ' ',
+  'fr': chars['NARROW NO-BREAK SPACE'],
+  'fr-sw': chars['NARROW NO-BREAK SPACE'],
 }
