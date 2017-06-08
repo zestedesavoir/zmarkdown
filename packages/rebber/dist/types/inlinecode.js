4617c9719c75541f70ab7a631394c3c0bc3231bc
Victor Felder <victor@draft.li>
2017-06-08 22:45:36 +0200

    rebber: inline-code fix tests

---
+++ packages/rebber/dist/types/inlinecode.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/* Expose. */
+module.exports = inlineCode;
+var escape = require('../escaper');
+function inlineCode(ctx, node) {
+  var finalCode = escape(node.value);
+  return '\\texttt{' + finalCode + '}';
+}
