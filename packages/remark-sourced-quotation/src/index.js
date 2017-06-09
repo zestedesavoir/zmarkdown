4f6b6e11c262dbee69043e2569679c22f37fa9dc
artragis <artragis@gmail.com>
2017-06-09 19:25:04 +0200

    try with figure

--- packages/remark-sourced-quotation/src/index.js
+++ packages/remark-sourced-quotation/src/index.js
@@ -5,22 +5,27 @@ function plugin () {
 }
 
 function transformer (tree) {
-  visit(tree, 'paragraph', visitor)
+  visit(tree, 'blockquote', visitor)
 }
 
 function visitor (node, index, parent) {
-  if (parent.type === 'blockquote' && !parent.author) {
+  const hasSource = node.children[node.children.length - 1].contains('Source:')
+  if (!node.author && node.children && hasSource) {
     const source = getSource(node)
-    parent.author = source
-    node.value.replace(source, '')
+    node.author = source
+    if (source) {
+      node.type = 'figure'
+      node.data.caption = source
+    }
   }
 }
 
-function getSource(node) {
-  const lines = node.value.split('\n')
+function getSource (node) {
+  const lines = node.children[node.children.length - 1].split('\n')
   let i = 0
   for (; i < lines.length && !lines[i].startsWith('Source:'); i++);
   if (i < lines.length && lines[i].startsWith('Source:')) {
+    node.children[node.children.length - 1].replace(lines[i], '')
     return lines[i].split(':')[1]
   }
   return undefined
