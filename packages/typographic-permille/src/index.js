0df89fe5b56dd55db3fd872584f8b0d5705b802f
AmarOk <contact@enconn.fr>
2017-06-11 16:57:14 -0400

    typo

--- packages/typographic-permille/src/index.js
+++ packages/typographic-permille/src/index.js
@@ -9,7 +9,7 @@ module.exports = (input = '', { locale } = {}) => {
   const result = input.replace(permillePattern, chars['PER MILLE SIGN'])
 
   if (Object.keys(db).includes(locale)) {
-    // If we need to replace spaces before the permille sign
+    // If we need to replace space before per mille signs
     const spaceBeforePermillePattern = /( )(\u2030)/g
     return result.replace(spaceBeforePermillePattern, `${db[locale]}$2`)
   }
