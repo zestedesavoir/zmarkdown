fb5585a712e037735e25c11f7c9f10d9196ad45f
Quentin Gliech <quentingliech@gmail.com>
2017-06-23 11:13:53 +0200

    use snapshots in rehype-abbr's tests

--- packages/rehype-abbr/__tests__/index.js
+++ packages/rehype-abbr/__tests__/index.js
@@ -1,40 +1,75 @@
-import {readdirSync as directory, readFileSync as file} from 'fs'
-import {join} from 'path'
+import dedent from 'dedent'
 import unified from 'unified'
 import reParse from 'remark-parse'
 import stringify from 'rehype-stringify'
 import remark2rehype from 'remark-rehype'
 
-const base = join(__dirname, 'fixtures')
-const specs = directory(base).reduce((tests, contents) => {
-  const parts = contents.split('.')
-  if (!tests[parts[0]]) {
-    tests[parts[0]] = {}
-  }
-  tests[parts[0]][parts[1]] = file(join(base, contents), 'utf-8')
-  return tests
-}, {})
+import abbr from '../src/'
 
-const entrypoints = [
-  '../dist',
-  '../src',
-]
+const render = text => unified()
+  .use(reParse)
+  .use(remark2rehype)
+  .use(abbr)
+  .use(stringify)
+  .processSync(text)
 
-entrypoints.forEach(entrypoint => {
-  const plugin = require(entrypoint)
+it('renders references', () => {
+  const { contents } = render(dedent`
+    This is an abbreviation: REF.
+    ref and REFERENCE should be ignored.
 
-  Object.keys(specs).filter(Boolean).forEach(name => {
-    const spec = specs[name]
+    Here is another one in a link: [FOO](http://example.com).
 
-    test(name, () => {
-      const {contents} = unified()
-        .use(reParse)
-        .use(remark2rehype)
-        .use(plugin)
-        .use(stringify)
-        .processSync(spec.fixture)
+    Here is the first one in a link: [REF](http://example.com).
 
-      expect(contents).toEqual(spec.expected)
-    })
-  })
+    *[REF]: Reference
+    *[FOO]: Reference
+  `)
+
+  expect(contents).toMatchSnapshot()
+})
+
+
+it('passes the first regression test', () => {
+  const { contents } = render(dedent`
+    The HTML specification is maintained by the W3C:\
+    [link](https://w3c.github.io/html/), this line had an abbr before link.
+
+    A line with [a link](http://example.com) before an abbr: HTML.
+
+    *[HTML]: Hyper Text Markup Language
+    *[W3C]:  World Wide Web Consortium
+  `)
+
+  expect(contents).toMatchSnapshot()
+})
+
+it('passes the second regression test', () => {
+  const { contents } = render(dedent`
+    The HTML specification is maintained by the W3C:\
+    [link](https://w3c.github.io/html/), this line had an abbr before **link** HTML.
+
+    A line with [a link](http://example.com) before an abbr: HTML.
+
+    *[HTML]: Hyper Text Markup Language
+    *[W3C]:  World Wide Web Consortium
+  `)
+
+  expect(contents).toMatchSnapshot()
+})
+
+it('passes the retro test', () => {
+  const { contents } = render(dedent`
+    An ABBR: "REF", ref and REFERENCE should be ignored.
+
+    The HTML specification is maintained by the W3C.
+
+    *[REF]: Reference
+    *[ABBR]: This gets overridden by the next one.
+    *[ABBR]: Abbreviation
+    *[HTML]: Hyper Text Markup Language
+    *[W3C]:  World Wide Web Consortium
+  `)
+
+  expect(contents).toMatchSnapshot()
 })
