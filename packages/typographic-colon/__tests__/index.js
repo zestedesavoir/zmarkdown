090342ba7e283bee84aec0a74a7ba2952a5f7df4
Victor Felder <victor@draft.li>
2017-07-22 00:35:24 +0200

    typographic-colon: fix test

--- packages/typographic-colon/__tests__/index.js
+++ packages/typographic-colon/__tests__/index.js
@@ -1,8 +1,4 @@
 /* eslint-disable no-irregular-whitespace */
-const entrypoints = [
-  '../dist',
-  '../src',
-]
 
 const chars = {
   'NARROW NO-BREAK SPACE': '\u202F'
@@ -12,19 +8,17 @@ const american = { locale: 'en-us' }
 const fr = { locale: 'fr' }
 const frCH = { locale: 'fr-sw' }
 
-entrypoints.forEach(entrypoint => {
-  const colon = require(entrypoint)
+const colon = require('../src')
 
-  test('should do nothing with no param at all', () => expect(colon()).toEqual(''))
+test('should do nothing with no param at all', () => expect(colon()).toEqual(''))
 
-  test('should do nothing if locale is undefined', () =>
-    expect(colon(`foo : bar`)).toEqual(`foo : bar`))
+test('should do nothing if locale is undefined', () =>
+  expect(colon(`foo : bar`)).toEqual(`foo : bar`))
 
-  test('should ignore locale not in DB', () =>
-    expect(colon(`foo : bar`, american)).toEqual(`foo : bar`))
+test('should ignore locale not in DB', () =>
+  expect(colon(`foo : bar`, american)).toEqual(`foo : bar`))
 
-  test('should handle fr[-*]', () => {
-    expect(colon(`foo : bar`, fr)).toEqual(`foo${chars['NARROW NO-BREAK SPACE']}: bar`)
-    expect(colon(`foo : bar`, frCH)).toEqual(`foo${chars['NARROW NO-BREAK SPACE']}: bar`)
-  })
+test('should handle fr[-*]', () => {
+  expect(colon(`foo : bar`, fr)).toEqual(`foo${chars['NARROW NO-BREAK SPACE']}: bar`)
+  expect(colon(`foo : bar`, frCH)).toEqual(`foo${chars['NARROW NO-BREAK SPACE']}: bar`)
 })
