0e1333eb693e9c90fb36353841b313d76ef58093
notapianokey <1752390+notapianokey@users.noreply.github.com>
2018-04-19 12:02:30 +0200

    remark-mark: copy from remark-kbd

---
+++ packages/remark-mark/__tests__/index.js
@@ -0,0 +1,56 @@
+import dedent from 'dedent'
+import unified from 'unified'
+import reParse from 'remark-parse'
+import remarkStringify from 'remark-stringify'
+import rehypeStringify from 'rehype-stringify'
+import remark2rehype from 'remark-rehype'
+import remarkCustomBlocks from '../../remark-custom-blocks'
+
+import plugin from '../src/'
+
+
+const render = text => unified()
+  .use(reParse, {
+    footnotes: true,
+  })
+  .use(remarkCustomBlocks, {
+    secret: 'spoiler',
+  })
+  .use(plugin)
+  .use(remark2rehype)
+  .use(rehypeStringify)
+  .processSync(text)
+
+const fixture = dedent`
+  Blabla !!ok!! kxcvj !!ok foo!! sdff
+
+  sdf !!!! df
+
+  sfdgs ! ! dfg !! dgsg ! qs
+
+  With two pipes: \!!key!! you'll get !!key!!.
+
+  It parses inline elements inside:
+
+  * !!hell[~~o~~](#he)?!!
+
+  but not block elements inside:
+
+  * !!hello: [[secret]]?!!
+`
+
+
+test('mark', () => {
+  const {contents} = render(fixture)
+  expect(contents).toMatchSnapshot()
+})
+
+test('to markdown', () => {
+  const {contents} = unified()
+    .use(reParse)
+    .use(remarkStringify)
+    .use(plugin)
+    .processSync(fixture)
+
+  expect(contents).toMatchSnapshot()
+})
---
+++ packages/remark-mark/src/index.js
@@ -0,0 +1,80 @@
+const whitespace = require('is-whitespace-character')
+
+const C_MARK = '!'
+const DOUBLE = '!!'
+
+function locator (value, fromIndex) {
+  const index = value.indexOf(DOUBLE, fromIndex)
+  return index
+}
+
+function plugin () {
+  function inlineTokenizer (eat, value, silent) {
+    if (
+      !this.options.gfm ||
+      value.charAt(0) !== C_MARK ||
+      value.charAt(1) !== C_MARK ||
+      value.startsWith(C_MARK.repeat(4)) ||
+      whitespace(value.charAt(2))
+    ) {
+      return
+    }
+
+    let character = ''
+    let previous = ''
+    let preceding = ''
+    let subvalue = ''
+    let index = 1
+    const length = value.length
+    const now = eat.now()
+    now.column += 2
+    now.offset += 2
+
+    while (++index < length) {
+      character = value.charAt(index)
+
+      if (
+        character === C_MARK &&
+        previous === C_MARK &&
+        (!preceding || !whitespace(preceding))
+      ) {
+
+        /* istanbul ignore if - never used (yet) */
+        if (silent) return true
+
+        return eat(DOUBLE + subvalue + DOUBLE)({
+          type: 'mark',
+          children: this.tokenizeInline(subvalue, now),
+          data: {
+            hName: 'mark',
+          },
+        })
+      }
+
+      subvalue += previous
+      preceding = previous
+      previous = character
+    }
+  }
+  inlineTokenizer.locator = locator
+
+  const Parser = this.Parser
+
+  // Inject inlineTokenizer
+  const inlineTokenizers = Parser.prototype.inlineTokenizers
+  const inlineMethods = Parser.prototype.inlineMethods
+  inlineTokenizers.mark = inlineTokenizer
+  inlineMethods.splice(inlineMethods.indexOf('text'), 0, 'mark')
+
+  const Compiler = this.Compiler
+
+  // Stringify
+  if (Compiler) {
+    const visitors = Compiler.prototype.visitors
+    visitors.mark = function (node) {
+      return `!!${this.all(node).join('')}!!`
+    }
+  }
+}
+
+module.exports = plugin
