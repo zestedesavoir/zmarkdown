c6410d76873adacfb74632459e3c82ed8e2a9bdb
heziode <heziode@protonmail.com>
2018-08-19 15:50:34 +0200

    feat(client-side): Add conf to build zmd for client side

---
+++ packages/zmarkdown/default.webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+
+module.exports = {
+  // entry: ['babel-polyfill', './src/entry'],
+  entry: ['babel-polyfill', './index'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env'],
+          },
+        },
+      },
+    ],
+  },
+}
