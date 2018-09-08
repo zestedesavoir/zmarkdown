197e8f13f987ac19641055ac6995d1aa0775aaef
heziode <heziode@protonmail.com>
2018-09-08 22:26:38 +0200

    chore(POC): Update test page

--- packages/zmarkdown/public/script.js
+++ packages/zmarkdown/public/script.js
@@ -2,14 +2,27 @@ Split(['#left', '#center', '#right'], {
   minSize: 200,
 })
 
+Split(['#left-top', '#left-bottom'], {
+  sizes: [75, 25],
+  direction: 'vertical'
+})
+
+Split(['#center-top', '#center-bottom'], {
+  sizes: [75, 25],
+  direction: 'vertical'
+})
+
 ZMarkdown.use(ZMarkdownZHTML);
 ZMarkdown.use(ZMarkdownZLatex);
 
 ZMarkdown.setDefaultModule("zhtml");
 
-const render = document.querySelector('#center > div')
+const ansiUp = new AnsiUp()
+const editor = document.querySelector('#left-top > textarea')
+const ast = document.querySelector('#left-bottom > pre > code')
+const render = document.querySelector('#center-top > div')
+const html = document.querySelector('#center-bottom > pre > code')
 const latex = document.querySelector('#right > pre')
-const editor = document.querySelector('#left > textarea')
 editor.addEventListener('input', update)
 
 document.addEventListener('DOMContentLoaded', update)
@@ -17,8 +30,11 @@ document.addEventListener('DOMContentLoaded', update)
 function update () {
   ZMarkdown.render(editor.value).then((vFile) => {
     render.innerHTML = vFile.toString().trim()
+    html.textContent = vFile.toString().trim()
   })
   ZMarkdown.render(editor.value, 'zlatex').then((vFile) => {
     latex.textContent = vFile.toString().trim()
   })
+  const mdast = ZMarkdown.parse(editor.value)
+  ast.innerHTML = ansiUp.ansi_to_html(ZMarkdown.getParser().inspect.color(mdast))
 }
