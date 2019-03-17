f2de2797fce66a8fdb03d3e24926ca00931af1de
Victor Felder <victor@draft.li>
2019-03-17 15:15:21 +0100

    mdast-util-split: refactor to get simpler code

--- packages/mdast-util-split/src/index.js
+++ packages/mdast-util-split/src/index.js
@@ -1,45 +1,16 @@
 import visit from 'unist-util-visit'
 
-const ROOT_HEADING_DEPTH = 1
+module.exports = splitAtDepth
 
-module.exports = function split (tree, config) {
-  const {
-    splitDepth = ROOT_HEADING_DEPTH,
-    conclusionAsProperty = false,
-  } = config
+function splitAtDepth (tree, {splitDepth = 1}) {
+  const splitter = new Splitter(splitDepth)
 
-  const firstHeading = find(tree, {type: 'heading', depth: ROOT_HEADING_DEPTH})
-  if (!firstHeading) {
-    throw new Error('No heading')
-  }
-  return splitAtDepth(
-    tree,
-    splitDepth, conclusionAsProperty
-  )
-}
-
-function splitAtDepth (tree, currentDepth, conclusionAsProperty) {
-
-  const splitter = new Splitter(currentDepth)
   visit(tree, null, (node, index, parent) => splitter.visit(node, index, parent))
-  let conclusion = null
-  if (conclusionAsProperty && splitter.subTrees.length > 1) {
-    conclusion = splitter.subTrees.pop().children
-  }
+
   return {
     introduction: splitter.introduction,
     trees: splitter.subTrees,
-    conclusion: conclusion,
   }
-}
-
-function find (tree, {type = 'heading', depth = ROOT_HEADING_DEPTH}) {
-  for (let i = 0; i < tree.children.length; i++) {
-    if (tree.children[i].type === type && tree.children[i].depth === depth) {
-      return tree.children[i]
-    }
-  }
-  return null
 }
 
 function newRootTree (children = []) {
@@ -58,15 +29,11 @@ class Splitter {
   }
 
   visit (node, index, parent) {
-    // when we are at root
     if (!parent) {
+      // we are at the root
       return
     }
-    if ((node.type !== 'heading' || node.depth !== this.depth) &&
-      parent.type === 'root' && this.lastIndex === -1) {
-      this.introduction.children.push(node)
-      return
-    }
+
     if (node.type === 'heading' && node.depth === this.depth) {
       this.lastIndex = index
       const subtree = {
@@ -74,6 +41,9 @@ class Splitter {
         children: newRootTree(),
       }
       this.subTrees.push(subtree)
+    } else if (parent.type === 'root' && this.lastIndex === -1) {
+      this.introduction.children.push(node)
+      return
     } else if (parent.type === 'root') {
       this.subTrees[this.subTrees.length - 1].children.children.push(node)
     }
