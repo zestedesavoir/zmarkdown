61be3428a47cc834d50f343eb6f92d119d1098fa
TitiAlone <titi_alone@gmx.fr>
2019-05-25 12:07:15 +0200

    Add new math tests

--- packages/zmarkdown/__tests__/sanitizer.tests.js
+++ packages/zmarkdown/__tests__/sanitizer.tests.js
@@ -27,7 +27,22 @@ describe('#math', () => {
   })
 
   it('do not oversanitize math - test frac', () => {
-    const rendered = renderString()('$$\frac{1+1}{x+y}$$')
+    const rendered = renderString()('$$\\frac{1+1}{x+y}$$')
+    expect(rendered).resolves.toMatchSnapshot()
+  })
+
+  it('do not oversanitize math - test sqrt', () => {
+    const rendered = renderString()('$$\\sqrt[3]{x^3 + y^3 + z^3}$$')
+    expect(rendered).resolves.toMatchSnapshot()
+  })
+
+  it('do not oversanitize math - test notin', () => {
+    const rendered = renderString()('$$\\notin$$')
+    expect(rendered).resolves.toMatchSnapshot()
+  })
+
+  it('do not oversanitize math - test overrightarrow', () => {
+    const rendered = renderString()('$$\\overrightarrow{AB}$$')
     expect(rendered).resolves.toMatchSnapshot()
   })
 })
