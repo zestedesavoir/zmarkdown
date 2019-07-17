ebb9caec9a96d2cc719c88354996f3a41e9af292
ache <ache@ache.one>
2019-07-17 09:31:58 +0200

    Update subpackages

--- packages/remark-images-download/dist/index.js
+++ packages/remark-images-download/dist/index.js
@@ -12,6 +12,10 @@ function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try
 
 function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
 
+require('core-js/stable');
+
+require('regenerator-runtime/runtime');
+
 var fileType = require('file-type');
 
 var fs = require('fs');
