977feecf18ebcc2f000d1d831c199e2bf05f56db
ache <ache@ache.one>
2019-07-24 17:58:37 +0200

    rehype-footnotes: Optimise speed

--- packages/rehype-footnotes-title/dist/index.js
+++ packages/rehype-footnotes-title/dist/index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var visit = require('unist-util-visit-parents');
+var visit = require('unist-util-visit');
 
 function plugin() {
   var title = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '';
@@ -9,10 +9,20 @@ function plugin() {
     visit(tree, 'element', visitor);
   }
 
-  function visitor(node, parents) {
-    if (node.tagName === 'a' && node.properties.className && node.properties.className.includes('footnote-backref') && parents.length > 2 && parents[parents.length - 2].tagName === 'li') {
-      var parent = parents[parents.length - 2];
-      var identifier = parent.properties.id.slice(3);
+  function visitor(node, index, parent) {
+    if (node.tagName === 'li' && node.properties.id) {
+      var aTag;
+      if (!node.children || !node.children.length) return;
+
+      if (node.children[node.children.length - 1].tagName === 'a') {
+        aTag = node.children[node.children.length - 1];
+      } else if (node.children[node.children.length - 2].tagName === 'p') {
+        var pTag = node.children[node.children.length - 2];
+        if (pTag.children[pTag.children.length - 1].tagName !== 'a') return;
+        aTag = pTag.children[pTag.children.length - 1];
+      } else return;
+
+      var identifier = node.properties.id.slice(3);
       var placeholderIndex = title.indexOf('$id');
       var thisTitle;
 
@@ -23,7 +33,7 @@ function plugin() {
       }
 
       if (!thisTitle) thisTitle = identifier;
-      node.properties.title = thisTitle;
+      aTag.properties.title = thisTitle;
     }
   }
 
--- packages/rehype-footnotes-title/src/index.js
+++ packages/rehype-footnotes-title/src/index.js
@@ -1,29 +1,35 @@
-const visit = require('unist-util-visit-parents')
+const visit = require('unist-util-visit')
 
 function plugin (title = '') {
   function transformer (tree) {
     visit(tree, 'element', visitor)
   }
 
-  function visitor (node, parents) {
-    if (
-      node.tagName === 'a' &&
-      node.properties.className &&
-      node.properties.className.includes('footnote-backref') &&
-      parents.length > 2 &&
-      parents[parents.length - 2].tagName === 'li'
-    ) {
-      const parent = parents[parents.length - 2]
-      const identifier = parent.properties.id.slice(3)
+  function visitor (node, index, parent) {
+    if (node.tagName === 'li' && node.properties.id) {
+      let aTag
+
+      if (!node.children || !node.children.length) return
+
+      if (node.children[node.children.length - 1].tagName === 'a') {
+        aTag = node.children[node.children.length - 1]
+      } else if (node.children[node.children.length - 2].tagName === 'p') {
+        const pTag = node.children[node.children.length - 2]
+        if (pTag.children[pTag.children.length - 1].tagName !== 'a') return
+        aTag = pTag.children[pTag.children.length - 1]
+      } else return
+
+      const identifier = node.properties.id.slice(3)
       const placeholderIndex = title.indexOf('$id')
       let thisTitle
+
       if (placeholderIndex !== -1) {
         thisTitle = title.split('')
         thisTitle.splice(placeholderIndex, 3, identifier)
         thisTitle = thisTitle.join('')
       }
       if (!thisTitle) thisTitle = identifier
-      node.properties.title = thisTitle
+      aTag.properties.title = thisTitle
     }
   }
   return transformer
