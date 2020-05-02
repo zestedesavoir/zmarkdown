60c9efed2bc9dd7f72c94de787cb66c775eefa9b
TitiAlone <talone@boxph.one>
2020-05-02 19:39:37 +0200

    Add postprocessor for hl_lines

--- packages/zmarkdown/postprocessors/html-code-highlight.js
+++ packages/zmarkdown/postprocessors/html-code-highlight.js
@@ -1,4 +1,15 @@
 const visit = require('unist-util-visit')
+const unified = require('unified')
+const rehypeParse = require('rehype-parse')
+const rehypeStringify = require('rehype-stringify')
+
+const toHtml = unified()
+  .use(rehypeStringify)
+  .stringify
+
+const fromHtml = unified()
+  .use(rehypeParse, {fragment: true})
+  .parse
 
 module.exports = () => (tree, vfile) => {
   visit(tree, 'element', node => {
@@ -13,6 +24,15 @@ module.exports = () => (tree, vfile) => {
     const lineNumberDiv = node.children[0]
     const codeBlock = node.children[1].children[0]
 
+    // Create a shadow element to remove all the crap from code.hljs
+    // This shadow element is renderer to html, and we'll add hll spans around
+    // matching lines.
+    // At the end, it is reparsed to HAST
+    const shadowElement = toHtml({
+      type:     'root',
+      children: codeBlock.children,
+    })
+
     // Get the lines to highlight relative to the first
     const hlLines = lineNumberDiv.children
       .reduce((acc, val, i) => {
@@ -22,5 +42,19 @@ module.exports = () => (tree, vfile) => {
 
         return acc
       }, [])
+
+    // Create a new code block in raw HTML, with lines highlighted
+    const newCodeBlock = shadowElement
+      .split('\n')
+      .map((line, i) => {
+        if (hlLines.includes(i)) {
+          return `<span class="hll">${line}</span>`
+        } else {
+          return line
+        }
+      })
+
+    // Insert new children
+    node.children[1].children[0].children = fromHtml(newCodeBlock.join('\n')).children
   })
 }
