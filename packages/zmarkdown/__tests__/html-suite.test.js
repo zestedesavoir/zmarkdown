11196a6c45b9e462387f3fca3b9e0bfd2f59da0c
TitiAlone <talone@boxph.one>
2020-05-02 20:40:19 +0200

    Add new tests for hl_lines and linenostart

--- packages/zmarkdown/__tests__/html-suite.test.js
+++ packages/zmarkdown/__tests__/html-suite.test.js
@@ -290,6 +290,100 @@ describe('code highlight special cases', () => {
     expect(result2).toBe(result1)
   })
 
+  it('supports hl_lines - spaced syntax', async () => {
+    const input = dedent `
+      \`\`\`python hl_lines="4 5"
+      def main():
+        print('It\'s amazing!')
+      
+      def unused():
+        print('Please use me!')
+      \`\`\`
+    `
+
+    const result = await renderString(input)
+    expect((result.match(/class="hll"/g) || []).length).toBe(4)
+    expect(result).toMatchSnapshot()
+  })
+
+  it('supports hl_lines - comma syntax', async () => {
+    const input = dedent `
+      \`\`\`python hl_lines=4,5
+      def main():
+        print('It\'s amazing!')
+      
+      def unused():
+        print('Please use me!')
+      \`\`\`
+    `
+
+    const result = await renderString(input)
+    expect((result.match(/class="hll"/g) || []).length).toBe(4)
+    expect(result).toMatchSnapshot()
+  })
+
+  it('supports hl_lines - interval syntax', async () => {
+    const input = dedent `
+      \`\`\`python hl_lines=1-2
+      def main():
+        print('It\'s amazing!')
+      
+      def unused():
+        print('Please use me!')
+      \`\`\`
+    `
+
+    const result = await renderString(input)
+    expect((result.match(/class="hll"/g) || []).length).toBe(4)
+    expect(result).toMatchSnapshot()
+  })
+
+  it('supports linenostart', async () => {
+    const input = dedent `
+      \`\`\`python linenostart=3
+      def main():
+        print('It\'s amazing!')
+      
+      def unused():
+        print('Please use me!')
+      \`\`\`
+    `
+
+    const result = await renderString(input)
+    expect(result).not.toContain('data-count="1"')
+    expect(result).toMatchSnapshot()
+  })
+
+  it('supports both hl_lines and linenostart', () => {
+    const input = dedent `
+      \`\`\`python hl_lines=4 linenostart=3
+      def main():
+        print('It\'s amazing!')
+      
+      def unused():
+        print('Please use me!')
+      \`\`\`
+    `
+
+    expect(renderString(input)).resolves.toMatchSnapshot()
+  })
+
+  it('discards unused attributes', async () => {
+    const input = dedent `
+      \`\`\`python none hl_lines
+      def main():
+        print('It\'s amazing!')
+      
+      def unused():
+        print('Please use me!')
+      \`\`\`
+    `
+
+    const result = await renderString(input)
+    expect(result).not.toContain('class="hll"')
+    expect(result).toMatchSnapshot()
+  })
+
   afterEach(() => {
     defaultHtmlConfig.disableTokenizers.internal = ['highlight']
     defaultHtmlConfig.bridge.handlers = {}
