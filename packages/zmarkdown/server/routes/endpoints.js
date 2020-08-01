26c99d13578cc1b1be2d9684ea42cc6c6e381914
TitiAlone <talone@boxph.one>
2020-08-01 12:49:06 +0200

    Fix for LaTeX document maximum size limit

--- packages/zmarkdown/server/routes/endpoints.js
+++ packages/zmarkdown/server/routes/endpoints.js
@@ -4,28 +4,29 @@ const express    = require('express')
 const controllerFactory = require('../factories/controller-factory')
 
 const router = express.Router()
+const limit = '300kb'
 
 router.post(
   '/html',
-  bodyParser.json({limit: '300kb'}),
+  bodyParser.json({limit}),
   controllerFactory('html'),
 )
 
 router.post(
   '/latex',
-  bodyParser.json({limit: '300kb'}),
+  bodyParser.json({limit}),
   controllerFactory('latex'),
 )
 
 router.post(
   '/epub',
-  bodyParser.json({limit: '300kb'}),
+  bodyParser.json({limit}),
   controllerFactory('epub'),
 )
 
 router.post(
   '/latex-document',
-  bodyParser.json({limit: '300kb'}),
+  bodyParser.json({limit: '3mb'}),
   controllerFactory('latex', require('../templates/latex-document')),
 )
 
