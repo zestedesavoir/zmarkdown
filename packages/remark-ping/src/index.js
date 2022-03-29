3e0301252a0e40cc5dd865ea21e99bcb48ffc63d
Stalone <talone@boxph.one>
2022-03-29 21:15:38 +0200

    [remark-ping] Do not take account of empty pings

--- packages/remark-ping/src/index.js
+++ packages/remark-ping/src/index.js
@@ -72,7 +72,7 @@ module.exports = function plugin ({
       username = username.slice(0, -fencedEndSequence.length)
     }
 
-    if (pingUsername(username) === true) {
+    if (pingUsername(username) === true && username.trim() !== '') {
       const url = userURL(username)
 
       return eat(eaten)({
