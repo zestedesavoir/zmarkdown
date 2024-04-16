9e5536a3b720029925a1f4eeea831af744558ada
TimonPeng <timonminipeng@gmail.com>
2024-04-17 00:42:59 +0800

    [remark-numbered-footnotes] Add labelTemplate option

--- packages/remark-numbered-footnotes/src/index.js
+++ packages/remark-numbered-footnotes/src/index.js
@@ -1,16 +1,44 @@
 const visit = require('unist-util-visit')
 
-function plugin () {
+function plugin ({labelTemplate = '{index}'} = {}) {
+  if (labelTemplate.indexOf('{index}') === -1) {
+    throw new Error('labelTemplate must contain {index}')
+  }
+
+  function transformer (tree) {
+    const footnotes = {}
+    visit(tree, 'footnote', convert)
+
+    visit(tree, 'footnoteDefinition', createIds(footnotes))
+
+    visit(tree, 'footnoteReference', replaceIds(footnotes))
+  }
+
+  function createIds (footnotes) {
+    return (node, index, parent) => {
+      const identifier = node.identifier
+
+      if (!footnotes.hasOwnProperty(identifier)) {
+        footnotes[identifier] = Object.keys(footnotes).length + 1
+      }
+      node.identifier = String(footnotes[identifier])
+      node.label = labelTemplate.replace('{index}', String(footnotes[identifier]))
+    }
+  }
+
+  function replaceIds (footnotes) {
+    return (node, index, parent) => {
+      const identifier = node.identifier
+
+      if (!footnotes.hasOwnProperty(identifier)) {
+        footnotes[identifier] = Object.keys(footnotes).length + 1
+      }
+      node.identifier = String(footnotes[identifier])
+      node.label = labelTemplate.replace('{index}', String(footnotes[identifier]))
+    }
+  }
+
   return transformer
-}
-
-function transformer (tree) {
-  const footnotes = {}
-  visit(tree, 'footnote', convert)
-
-  visit(tree, 'footnoteDefinition', createIds(footnotes))
-
-  visit(tree, 'footnoteReference', replaceIds(footnotes))
 }
 
 function convert (node, index, parent) {
@@ -28,30 +56,6 @@ function convert (node, index, parent) {
     identifier: id,
   }
   parent.children.splice(index, 1, footnoteReference, footnoteDefinition)
-}
-
-function createIds (footnotes) {
-  return (node, index, parent) => {
-    const identifier = node.identifier
-
-    if (!footnotes.hasOwnProperty(identifier)) {
-      footnotes[identifier] = Object.keys(footnotes).length + 1
-    }
-    node.identifier = String(footnotes[identifier])
-    node.label = String(footnotes[identifier])
-  }
-}
-
-function replaceIds (footnotes) {
-  return (node, index, parent) => {
-    const identifier = node.identifier
-
-    if (!footnotes.hasOwnProperty(identifier)) {
-      footnotes[identifier] = Object.keys(footnotes).length + 1
-    }
-    node.identifier = String(footnotes[identifier])
-    node.label = String(footnotes[identifier])
-  }
 }
 
 function autoId (node) {
