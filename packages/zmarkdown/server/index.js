e0e18b5f371c5ec1e57f0f33e9d97d71c227a899
Lika Kavkasidze <stalone@boxph.one>
2024-04-20 20:53:12 +0200

    [meta] Fix Sentry error reporting

--- packages/zmarkdown/server/index.js
+++ packages/zmarkdown/server/index.js
@@ -6,17 +6,15 @@ const path = require('path')
 
 const zmdVersion = require('../package.json').version
 
+const app = express()
+
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
   release: process.env.SENTRY_RELEASE || zmdVersion,
   environment: process.env.SENTRY_ENVIRONMENT || process.env.ZDS_ENVIRONMENT
 })
 
-const app = express()
-
-// Sentry request handling
 app.use(Sentry.Handlers.requestHandler())
-
 app.use(cors())
 
 // Expose an image for tests
@@ -28,8 +26,20 @@ if (process.env.ZMD_ENV !== 'production') {
 app.use('/', require('./routes/endpoints'))
 app.use('/munin', require('./routes/munin'))
 
-// Sentry error handling
+// Sentry error handler
 app.use(Sentry.Handlers.errorHandler())
+
+// Custom error handler, called only if a route throws
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    type: 'InternalServerError',
+    statusCode: 500,
+    errorMessage: err.toString(),
+    uniqueId: res.sentry
+  })
+  // eslint-disable-next-line no-useless-return
+  return
+})
 
 const server = app.listen(process.env.PORT || 27272, () => {
   const host = server.address().address
